ba02d700ed1306078b27e3b67f85d525c635712a
Mariusz Rak <mariusz@scrive.com>
2019-03-11 13:33:38 +0100

    Add timeout also to set and add calls

--- src/Mixpanel/Engage.hs
+++ src/Mixpanel/Engage.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Mixpanel.Engage (Property(..), set, add, MixpanelResult(..))
 
 where
 
-import Network.HTTP hiding (Custom)
+import Network.HTTP.Client
+import Network.HTTP.Client.TLS
+import Network.HTTP.Simple (getResponseStatusCode, getResponseBody, setRequestMethod)
 import Text.JSON as J
 import Text.JSON.Gen
 import qualified Text.JSON.Gen as J
@@ -12,6 +15,7 @@ import Control.Monad
 import Data.Time.Format
 import Data.ByteString.Base64 as B64
 import Data.ByteString.UTF8 as B
+import Data.ByteString.Lazy.UTF8 as BU
 
 import Mixpanel.Result
 import Mixpanel.Properties
@@ -49,14 +53,21 @@ set token distinctid properties = do
       jsb64 = B.toString $ B64.encode $ B.fromString jsstring
 
   let url = "http://api.mixpanel.com/engage?data=" ++ jsb64
-  eres <- simpleHTTP (postRequest url)
-  
-  case eres of
-    Left ce -> return $ HTTPError $ show ce
-    Right res -> case rspBody res of
-      "0" -> return $ MixpanelError $ show $ rspReason res
-      "1" -> return Success
-      r -> return $ HTTPError $ "Don't understand response. Should be '0' or '1'. Got: " ++ r
+
+  reqManager <- newTlsManager
+  let timeout = responseTimeoutMicro $ 10 {- secs -} * 1000000
+      req = (setRequestMethod "POST") $
+              (parseRequest_ url) { responseTimeout = timeout }
+
+  resp <- httpLbs req reqManager
+  if (getResponseStatusCode resp == 200)
+    then do
+      case (getResponseBody resp) of
+        "0" -> return $ MixpanelError $ "Mixpanel response is 0"
+        "1" -> return Success
+        r -> return $ HTTPError $ "Don't understand response. Should be '0' or '1' but got: " ++ BU.toString r
+    else return $ HTTPError $ show $ getResponseStatusCode resp
+
 
 add :: String -> String -> [Property] -> IO MixpanelResult
 add token distinctid properties = do
@@ -70,11 +81,18 @@ add token distinctid properties = do
       jsb64 = B.toString $ B64.encode $ B.fromString jsstring
 
   let url = "http://api.mixpanel.com/engage?data=" ++ jsb64
-  eres <- simpleHTTP (postRequest url)
-  
-  case eres of
-    Left ce -> return $ HTTPError $ show ce
-    Right res -> case rspBody res of
-      "0" -> return $ MixpanelError $ show $ rspReason res
-      "1" -> return Success
-      r -> return $ HTTPError $ "Don't understand response. Should be '0' or '1'. Got: " ++ r
+  reqManager <- newTlsManager
+  let timeout = responseTimeoutMicro $ 10 {- secs -} * 1000000
+      req = (setRequestMethod "POST") $
+              (parseRequest_ url) { responseTimeout = timeout }
+
+  resp <- httpLbs req reqManager
+  if (getResponseStatusCode resp == 200)
+    then do
+      case (getResponseBody resp) of
+        "0" -> return $ MixpanelError $ "Mixpanel response is 0"
+        "1" -> return Success
+        r -> return $ HTTPError $ "Don't understand response. Should be '0' or '1' but got: " ++ BU.toString r
+    else return $ HTTPError $ show $ getResponseStatusCode resp
+
+
